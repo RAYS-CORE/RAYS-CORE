a5071c201b64a18c7db5da9586283a2e1b064a54
Sam Reedh Bhuyan <samreedhbhuyan@example.com>
2026-07-13 00:52:41 +0530

    fix(rayspy): bump version to 1.7.2 for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rays-core",
-    version="1.7.1",
+    version="1.7.2",
     description="RAYS-CORE — AI-Powered Development Assistant",
     author="Samreedh Bhuyan",
     url="https://github.com/markknoffler/RAYS-CORE-CLI",
